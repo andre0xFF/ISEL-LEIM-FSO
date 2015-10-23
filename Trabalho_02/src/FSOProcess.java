ccf335dfd86a1e479d20dec13b4ac4345182a745
andrewfonseca <afbfonseca@gmail.com>
2015-10-23 08:56:18 +0100

    Added empty constructor

--- Trabalho_02/src/FSOProcess.java
+++ Trabalho_02/src/FSOProcess.java
@@ -18,39 +18,6 @@ public class FSOProcess {
 	public FSOProcess(String path, Process process) {
 		setProcess(path, process);
 	}
-	/*
-	public Boolean openProcess(String path) {
-		if(path.endsWith(".jar"))
-			return runJavaProcess(path);
-		else if(path.endsWith(".exe"))
-			return runWinProcess(path);
-		
-		return false;
-	}
-
-	private Boolean runWinProcess(String path) {
-		ProcessBuilder pb = new ProcessBuilder(path);
-		process = null;
-		
-		try {
-			process = pb.start();
-			this.path = path;
-			return true;
-		} catch (IOException e) {
-			return false;
-		}
-	}
 	
-	public Boolean runJavaProcess(String path) {
-		process = null;
-		
-		try {
-			process = Runtime.getRuntime().exec("java -jar " + path);
-			this.path = path;
-			return true;
-		} catch (IOException e) {
-			return false;
-		}
-	}
-	*/
+	public FSOProcess() { }
 }
